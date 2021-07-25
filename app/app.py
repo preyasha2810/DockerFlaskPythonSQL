b5eb2b55dca262c82c999fa1bfceb75f28e11b95
Preyasha <preyashapatel2328@gmail.com>
2021-07-25 17:16:37 -0400

    Task: updated previous file

--- app/app.py
+++ app/app.py
@@ -26,11 +26,19 @@ def cities_import() -> List[Dict]:
     return result
 
 
-@app.route('/')
-def index() -> str:
-    js = json.dumps(cities_import())
-    resp = Response(js, status=200, mimetype='application/json')
-    return resp
+
+    @app.route('/')
+    def index():
+        user = {'username': 'Miguel'}
+        cities_data = cities_import()
+
+        return render_template('index.html', title='Home', user=user, cities=cities_data)
+
+    @app.route('/api/cities')
+    def cities() -> str:
+        js = json.dumps(cities_import())
+        resp = Response(js, status=200, mimetype='application/json')
+        return resp
 
 
 if __name__ == '__main__':
